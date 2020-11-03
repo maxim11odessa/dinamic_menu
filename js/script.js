dfc5d15b4e510cecf2c32da2cd239db74d009c94
Max <maxim11.odessa@gmail.com>
2020-11-03 21:12:43 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const tabs = document.querySelectorAll('.tabheader__item'),
+         tabsContent = document.querySelectorAll('.tabcontent'),
+         tabsParent = document.querySelector('.tabheader__items');
+
+         function hideTabContent () {
+            tabsContent.forEach(item => {
+               item.style.display = 'none';
+            });
+
+            tabs.forEach(item => {
+               item.classList.remove('tabheader__item_active');
+            });
+         }
+
+         function showTabContent (i = 0) {
+            tabsContent[i].style.display = 'block';
+            tabs[i].classList.add('tabheader__item_active');
+         }
+
+         tabsParent.addEventListener('click', (ivent) => {
+            const target = ivent.target;
+
+            if (target && target.classList.contains('tabheader__item')){
+               tabs.forEach((item, i) => {
+                  if (target == item) {
+                     hideTabContent();
+                     showTabContent(i);
+                  }
+               });
+            }
+         });
+
+         hideTabContent();
+         showTabContent();
+});
